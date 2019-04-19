6da2c3841a32f63d05fab4836b3510e21c6e5fcb
alanchen <chenyan900520@126.com>
2019-04-19 11:28:12 +0800

    完成Navigation

---
+++ app/src/main/java/com/netelis/retailpos/fragment/MainFragment.java
@@ -0,0 +1,29 @@
+package com.netelis.retailpos.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.netelis.retailpos.R;
+
+
+public class MainFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.findViewById(R.id.btn_go_second).setOnClickListener(v -> Navigation.findNavController(view).navigate(R.id.action_mainFragment_to_secondFragment));
+    }
+
+}
